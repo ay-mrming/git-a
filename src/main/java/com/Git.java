1976a35525b818e6c99e3ff11702e11f1a8c8e4a
ay <nhao@test.com>
2023-12-13 23:46:57 +0800

    这是第三次提交

--- src/main/java/com/Git.java
+++ src/main/java/com/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("测试 idea git使用");
         System.out.println("完了，我被美女包围了！~");
+        System.out.println("完了，你成了百万富翁！~");
     }
 }
