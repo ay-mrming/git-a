e0b4b0c9c6a28b5f5cf4d89a43c8eecdfb677989
ay <nhao@test.com>
2023-12-14 13:00:57 +0800

    这是hot-fix分支修改

--- src/main/java/com/Git.java
+++ src/main/java/com/Git.java
@@ -5,5 +5,6 @@ public class Git {
         System.out.println("测试 idea git使用");
         System.out.println("完了，我被美女包围了！~");
         System.out.println("完了，你成了百万富翁！~");
+        System.out.println("这是hot-fix分支！~");
     }
 }
