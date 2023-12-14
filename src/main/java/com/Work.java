b7811ad7f27616ddbb3ea3c1a2d3834f869441c7
ay <nhao@test.com>
2023-12-14 13:22:33 +0800

    dasda

--- src/main/java/com/Work.java
+++ src/main/java/com/Work.java
@@ -4,5 +4,6 @@ public class Work {
     public static void doing(String work){
         System.out.println("我正在做"+work);
         System.out.println("这是hot-fix分支...");
+        System.out.println("dsadsadasdsa");
     }
 }
