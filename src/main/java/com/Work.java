c80fd9b6e625d73e35e6e93ea204cccb7691242f
ay-mrming <153702228+ay-mrming@users.noreply.github.com>
2023-12-14 19:34:25 +0800

    Update Work.java
    云端

--- src/main/java/com/Work.java
+++ src/main/java/com/Work.java
@@ -4,10 +4,6 @@ public class Work {
     public static void doing(String work){
         System.out.println("我正在做"+work);
         System.out.println("这是hot-fix分支...");
-        System.out.println("dsadsadasdsa");
-        System.out.println("dhaskjdhaskj");
-        System.out.println("dsadasdsadsadsadsa");
-        System.out.println("dghaskdhjksahdjkasdhjsa");
-        System.out.println("dshakjdhsakjdhsjka");
+        System.out.println("这是云端库..");
     }
 }
